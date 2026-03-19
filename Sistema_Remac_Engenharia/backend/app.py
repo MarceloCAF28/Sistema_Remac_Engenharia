964706664f071e0bffbdaed99ae8103cb903cd17
Marcelo Carvalho De Assis Filho <mcaffilho28@gmail.com>
2026-03-18 22:28:34 -0300

    Add files via upload

--- Sistema_Remac_Engenharia/backend/app.py
+++ Sistema_Remac_Engenharia/backend/app.py
@@ -5,6 +5,9 @@ import os
 import pandas as pd
 from io import BytesIO
 import traceback
+import os
+from flask import Flask
+from flask_cors import CORS
 
 # Importar db e modelos
 from database import db
@@ -2944,3 +2947,5 @@ if __name__ == '__main__':
     print("   - Relatórios Excel")
     print("=" * 60)
     app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
